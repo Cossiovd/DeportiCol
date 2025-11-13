4b5c49792ecb17831af6abefaa15f4ce4bc05e89
cossiovd <dcossio@poligran.edu.co>
2025-11-13 09:07:44 -0500

    Ajustes header y footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="bg-blue-900 text-gray-200 py-6 text-center">
-      <p>© 2025 DeportiCol – Impulsando el deporte colombiano</p>
+      <p>© 2025 DeportiCol – Impulsando el deporte Colombiano</p>
     </footer>
   );
 }
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,11 +1,14 @@
 import logo from "../assets/logo.svg";
+import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
     <header className="flex justify-between items-center px-8 py-4 bg-white shadow-sm">
       <div className="flex items-center gap-2">
+        <Link to="/" className="flex items-center gap-2">
         <img src={logo} alt="Logo DeportiCol" className="h-8 w-auto" />
         <h1 className="text-lg font-semibold text-[#1C398E]">DeportiCol</h1>
+        </Link>
       </div>
       <button className="border border-blue-600 text-blue-600 px-4 py-2 rounded-lg hover:bg-blue-600 hover:text-white transition">
         Iniciar sesión
